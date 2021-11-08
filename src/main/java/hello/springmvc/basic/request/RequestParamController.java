00b2725e6d7f91eb1ea02744f9c4faa1571cf01c
Timo <developer.timo@gmail.com>
2021-11-08 21:18:02 +0900

    [스프링 MVC - 기본 기능] HTTP 요청 파라미터 - @RequestParam > 파라미터 필수 여부

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -51,4 +51,14 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
 }
